4e289a37154daa2ee44a60962f7fbdab439e2ecf
KadeDr <dressingkade@gmail.com>
2025-02-14 10:00:36 -0600

    Fixed the parallax showing over the other elements on the top image

--- Javascript/main.js
+++ Javascript/main.js
@@ -1,3 +1,8 @@
 function ChangeLink(link) {
     window.location.replace(link)
 }
+
+window.addEventListener('scroll', function () {
+    let scrolled = window.scrollY;
+    document.querySelector('.TopImage').style.transform = `translateY(${scrolled * 0.5}px)`;
+});
