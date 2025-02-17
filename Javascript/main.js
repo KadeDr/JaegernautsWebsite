8401eccbaad6930f52429bfe3ef936a42fbb3de5
KadeDr <dressingkade@gmail.com>
2025-02-17 16:39:35 -0600

    DROPDOWNNNNNNN UPDATEEEEE HOLY MOLY :grin: :smiling_imp:

--- Javascript/main.js
+++ Javascript/main.js
@@ -1,5 +1,28 @@
-function Redirect(link) {
+function NewTab(link) {
     window.open(link)
+}
+
+function Redirect(link) {
+    window.location.href = link
+}
+
+var dropped = false;
+
+function ToggleDropdown() {
+    dropped = !dropped;
+    if (dropped) {
+        document.getElementById("InnerDropdown").setAttribute("style",
+            "display: block; margin: 0px; padding: 0px; position: absolute; left: 0px; width: 100%; background-color: #FFFFFF"
+        )
+        document.getElementById("OuterDropdown").setAttribute("style",
+            "display: block; margin: 0px; padding: 0px;"
+        )
+    }
+    else if (!dropped) {
+        document.getElementById("InnerDropdown").setAttribute("style",
+            "display: none"
+        )
+    }
 }
 
 window.addEventListener('scroll', function () {
